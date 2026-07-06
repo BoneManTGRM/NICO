d335e9faceed542c8a71b1f77455e8a7637c3929
Reparodynamics <papadenico@yahoo.com>
2026-07-05 18:29:30 -0600

    Route assess CLI to no-server mode

--- nico/__main__.py
+++ nico/__main__.py
@@ -1,4 +1,11 @@
-from nico.cli import main
+import sys
+
+from nico.cli import main as cli_main
+from nico.no_server_assessment import main as assess_main
+
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1 and sys.argv[1] == "assess":
+        assess_main(sys.argv[2:])
+    else:
+        cli_main()
