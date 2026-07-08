8c1e48290022fec0331f6090479206ebce38e711
Reparodynamics <papadenico@yahoo.com>
2026-07-08 12:45:00 -0600

    Install runtime truth patches

--- nico/__init__.py
+++ nico/__init__.py
@@ -2,8 +2,10 @@
 
 from nico.hosted_metadata_auth import install_metadata_auth_for_hosted_assessment
 from nico.hosted_dependency_normalization import patch_hosted_assessment_dependency_parsing
+from nico.report_truth_runtime_patch import patch_final_report_consistency
 
 install_metadata_auth_for_hosted_assessment()
 patch_hosted_assessment_dependency_parsing()
+patch_final_report_consistency()
 
 __version__ = "0.1.0"
