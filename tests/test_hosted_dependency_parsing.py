d4aa3c01a801cb1bdcfad2c72cf31b5682c52d54
Reparodynamics <papadenico@yahoo.com>
2026-07-08 12:09:20 -0600

    Cover PyJWT extras normalization without direct crypto pin

--- tests/test_hosted_dependency_parsing.py
+++ tests/test_hosted_dependency_parsing.py
@@ -1,12 +1,11 @@
 from nico.hosted_assessment import parse_requirements
 
 
-def test_hosted_requirements_do_not_send_extras_as_osv_package_names():
-    deps = parse_requirements("PyJWT==2.13.0\ncryptography==46.0.3\n")
+def test_hosted_requirements_normalize_extras_before_osv_package_lookup():
+    deps = parse_requirements("PyJWT[crypto]==2.13.0\n")
     names = {item["name"] for item in deps}
     versions = {item["name"]: item["version"] for item in deps}
 
     assert "PyJWT" in names
     assert "PyJWT[crypto]" not in names
     assert versions["PyJWT"] == "2.13.0"
-    assert versions["cryptography"] == "46.0.3"
