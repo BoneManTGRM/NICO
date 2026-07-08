1f7b375257809888a9c0bf02ae21ef0cf13418da
Reparodynamics <papadenico@yahoo.com>
2026-07-08 13:34:17 -0600

    Install deployed truth-source guard

--- nico/__init__.py
+++ nico/__init__.py
@@ -3,9 +3,11 @@
 from nico.hosted_metadata_auth import install_metadata_auth_for_hosted_assessment
 from nico.hosted_dependency_normalization import patch_hosted_assessment_dependency_parsing
 from nico.report_truth_runtime_patch import patch_final_report_consistency
+from nico.deployed_truth_source import install_deployed_truth_source
 
 install_metadata_auth_for_hosted_assessment()
 patch_hosted_assessment_dependency_parsing()
 patch_final_report_consistency()
+install_deployed_truth_source()
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
