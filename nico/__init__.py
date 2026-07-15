02556e231659a3ab19b220c7b5974636ddc52f38
Reparodynamics <papadenico@yahoo.com>
2026-07-15 08:30:34 -0600

    Bind professional PDF after final intelligence rebuild

--- nico/__init__.py
+++ nico/__init__.py
@@ -7,6 +7,7 @@ from nico.report_final_qa import patch_polish_express_result_for_final_qa
 from nico.report_truth_runtime_patch import patch_final_report_consistency
 from nico.report_pdf_display_patch import apply_pdf_display_patch
 from nico.report_intelligence_professional_pdf import install_professional_report_intelligence_pdf
+from nico.report_intelligence_final_pdf_binding import install_report_intelligence_final_pdf_binding
 from nico.hosted_report_regression_patch import install_hosted_report_regression_patch
 from nico.hosted_evidence_execution_patch import install_hosted_evidence_execution_patch
 from nico.hosted_scanner_runtime_diagnostics import install_hosted_scanner_runtime_diagnostics_route
@@ -87,5 +88,6 @@ install_post_polish_score_reconciliation_patch()
 install_express_completion_score_binding()
 install_hosted_report_intelligence_enrichment()
 install_report_intelligence_accuracy_patch()
+install_report_intelligence_final_pdf_binding()
 
 __version__ = "0.1.0"
