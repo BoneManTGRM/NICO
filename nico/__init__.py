353bdb51edf4a3a301609780e4266dfc5558085c
Reparodynamics <papadenico@yahoo.com>
2026-07-15 05:05:48 -0600

    Install post-polish score reconciliation last

--- nico/__init__.py
+++ nico/__init__.py
@@ -38,6 +38,7 @@ from nico.complexity_score_integrity_install_compat import install_complexity_sc
 from nico.complexity_density_confidence_patch import install_complexity_density_confidence_patch
 from nico.complexity_function_measurement_patch import install_complexity_function_measurement_patch
 from nico.final_score_reconciliation_patch import install_final_score_reconciliation_patch
+from nico.post_polish_score_reconciliation_patch import install_post_polish_score_reconciliation_patch
 
 install_metadata_auth_for_hosted_assessment()
 patch_hosted_assessment_dependency_parsing()
@@ -77,5 +78,6 @@ install_complexity_score_integrity_reentry_compat()
 install_complexity_density_confidence_patch()
 install_complexity_function_measurement_patch()
 install_final_score_reconciliation_patch()
+install_post_polish_score_reconciliation_patch()
 
 __version__ = "0.1.0"
