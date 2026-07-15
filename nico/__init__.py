eefbcf940347438dafc3031fdada1396e80cd4ce
Reparodynamics <papadenico@yahoo.com>
2026-07-15 06:42:31 -0600

    Install hosted report intelligence enrichment

--- nico/__init__.py
+++ nico/__init__.py
@@ -40,6 +40,7 @@ from nico.complexity_function_measurement_patch import install_complexity_functi
 from nico.final_score_reconciliation_patch import install_final_score_reconciliation_patch
 from nico.post_polish_score_reconciliation_patch import install_post_polish_score_reconciliation_patch
 from nico.express_completion_score_binding import install_express_completion_score_binding
+from nico.hosted_report_intelligence_enrichment import install_hosted_report_intelligence_enrichment
 
 install_metadata_auth_for_hosted_assessment()
 patch_hosted_assessment_dependency_parsing()
@@ -81,5 +82,6 @@ install_complexity_function_measurement_patch()
 install_final_score_reconciliation_patch()
 install_post_polish_score_reconciliation_patch()
 install_express_completion_score_binding()
+install_hosted_report_intelligence_enrichment()
 
 __version__ = "0.1.0"
