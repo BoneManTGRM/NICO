109afd87735524124db76109ddd845579eb26c7a
Reparodynamics <papadenico@yahoo.com>
2026-07-16 09:03:33 -0600

    Install section-specific Mid quality blocker display

--- nico/__init__.py
+++ nico/__init__.py
@@ -67,6 +67,7 @@ from nico.storage_serialization_safety import install_storage_serialization_safe
 from nico.assessment_persistence_truth_patch import install_assessment_persistence_truth
 from nico.mid_terminal_truth_patch import install_mid_terminal_truth_patch
 from nico.mid_terminal_truth_compat import install_mid_terminal_truth_compat
+from nico.mid_quality_issue_display_patch import install_mid_quality_issue_display_patch
 
 install_storage_serialization_safety()
 install_runtime_heartbeat_atomic_patch()
@@ -135,5 +136,6 @@ install_report_quality_gate()
 install_report_quality_gate_compat()
 install_mid_terminal_truth_patch()
 install_mid_terminal_truth_compat()
+install_mid_quality_issue_display_patch()
 
 __version__ = "0.1.0"
