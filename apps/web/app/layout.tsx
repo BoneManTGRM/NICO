849b5c435255e503d75d05b58b7445d7f9588131
Reparodynamics <papadenico@yahoo.com>
2026-07-24 08:04:07 -0600

    Avoid layout conflicts while preserving workflow styles

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -12,7 +12,6 @@ import AssessmentExpressRecoveryActions from "./AssessmentExpressRecoveryActions
 import AssessmentExpressRecoveryGuard from "./AssessmentExpressRecoveryGuard";
 import AssessmentFailureEvidencePanel from "./AssessmentFailureEvidencePanel";
 import AssessmentFailureResponseBridge from "./AssessmentFailureResponseBridge";
-import AssessmentFinalReviewAction from "./AssessmentFinalReviewAction";
 import AssessmentFinalGateAuthoritativeGuard from "./AssessmentFinalGateAuthoritativeGuard";
 import AssessmentHomeRedirect from "./AssessmentHomeRedirect";
 import AssessmentProgressIntegrityGuard from "./AssessmentProgressIntegrityGuard";
@@ -26,7 +25,6 @@ import GenericRepositoryExample from "./GenericRepositoryExample";
 import LegacyFullRunRedirect from "./LegacyFullRunRedirect";
 import {MidWorkspaceProvider} from "./MidWorkspaceContext";
 import OperationsPreloadGuard from "./OperationsPreloadGuard";
-import OperatorWorkspaceLocale from "./OperatorWorkspaceLocale";
 import PrimaryNavigation from "./PrimaryNavigation";
 import ReportPresentationGuard from "./ReportPresentationGuard";
 import RetainerAutoEvidenceLauncher from "./RetainerAutoEvidenceLauncher";
@@ -62,7 +60,6 @@ export default function RootLayout({children}: {children: ReactNode}) {
           <AssessmentApiTransportBridge />
           <AssessmentExactCommitTransport />
           <AssessmentFailureResponseBridge />
-          <AssessmentFinalReviewAction />
           <AssessmentHomeRedirect />
           <LegacyFullRunRedirect />
           <AssessmentRequestGuard />
@@ -71,7 +68,6 @@ export default function RootLayout({children}: {children: ReactNode}) {
           <OperationsPreloadGuard />
           <RetainerAutoEvidenceLauncher />
           <WorkspaceClarityRepair />
-          <OperatorWorkspaceLocale />
           <PrimaryNavigation />
           <WorkflowCallout />
           <GenericRepositoryExample />
