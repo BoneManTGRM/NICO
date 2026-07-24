87638f7270f0b7d06562f3841ae3e2f71123d3f9
Reparodynamics <papadenico@yahoo.com>
2026-07-24 05:24:51 -0600

    Strengthen Spanish locale document metadata

--- apps/web/app/es/assessment/SpanishDocumentLanguage.tsx
+++ apps/web/app/es/assessment/SpanishDocumentLanguage.tsx
@@ -4,12 +4,23 @@ import {useEffect} from "react";
 
 export default function SpanishDocumentLanguage() {
   useEffect(() => {
-    const previous = document.documentElement.lang;
+    const previousLanguage = document.documentElement.lang;
+    const previousDirection = document.documentElement.dir;
+    const previousHtmlLocale = document.documentElement.dataset.nicoLocale;
+    const previousBodyLocale = document.body.dataset.nicoLocale;
+
     document.documentElement.lang = "es-MX";
+    document.documentElement.dir = "ltr";
+    document.documentElement.dataset.nicoLocale = "es-MX";
     document.body.dataset.nicoLocale = "es-MX";
+
     return () => {
-      document.documentElement.lang = previous || "en";
-      delete document.body.dataset.nicoLocale;
+      document.documentElement.lang = previousLanguage || "en";
+      document.documentElement.dir = previousDirection || "ltr";
+      if (previousHtmlLocale) document.documentElement.dataset.nicoLocale = previousHtmlLocale;
+      else delete document.documentElement.dataset.nicoLocale;
+      if (previousBodyLocale) document.body.dataset.nicoLocale = previousBodyLocale;
+      else delete document.body.dataset.nicoLocale;
     };
   }, []);
 
