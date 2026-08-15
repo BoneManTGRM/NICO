352328ea70fa0acb440b69f2d3c3b335d51cefca
Reparodynamics <papadenico@yahoo.com>
2026-08-14 19:08:54 -0600

    Wire live Spanish assessment localization

--- apps/web/app/assessment/AssessmentPage.tsx
+++ apps/web/app/assessment/AssessmentPage.tsx
@@ -2,6 +2,7 @@ import AssessmentWorkspace from "./AssessmentWorkspace";
 import AssessmentRuntimeTruthRepair from "./AssessmentRuntimeTruthRepair";
 import AssessmentMetricDisplayV44 from "./AssessmentMetricDisplayV44";
 import AssessmentHydrationContract from "./AssessmentHydrationContract";
+import AssessmentDynamicSpanishLocalization from "./AssessmentDynamicSpanishLocalization";
 import "./assessment-inline-readiness.css";
 
 export const ASSESSMENT_CLIENT_COPY_CONTRACT = "expert-engagement-hydrated-v1";
@@ -19,6 +20,7 @@ export default function AssessmentPage({locale = "en"}: {locale?: "en" | "es-MX"
   const exactReleaseSha = releaseSha();
   return <>
     <AssessmentRuntimeTruthRepair />
+    <AssessmentDynamicSpanishLocalization />
     <AssessmentMetricDisplayV44 />
     <AssessmentWorkspace locale={locale} />
     <AssessmentHydrationContract
