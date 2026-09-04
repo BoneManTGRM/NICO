1e339c58dcce2caed6ba78ffd1b8d496d79deb97
Reparodynamics <papadenico@yahoo.com>
2026-09-04 08:45:45 -0700

    Use fixed English specialist entrypoint

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 import {redirect} from "next/navigation";
 
 export default function Page() {
-  redirect("/specialist-login?next=%2Fassessment%3Ftier%3Dcomprehensive%23assessment");
+  redirect("/specialist-login");
 }
